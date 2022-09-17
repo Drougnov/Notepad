61cbdc13d87d1d70b29c7e692f6053e0616aba0c
RockyBaura <251drougnov@gmail.com>
2022-09-17 14:36:21 +0600

    USed localStorage to save note

--- src/App.js
+++ src/App.js
@@ -5,38 +5,67 @@ import Split from 'react-split';
 import { nanoid } from 'nanoid';
 
 export default function App(){
-  const [notes, setNotes] = React.useState([]);
-  const [currentNoteId, setCurrentNoteId] = React.useState((notes[0] && notes[0].id) || '')
+  const [notes, setNotes] = React.useState(JSON.parse(localStorage.getItem("notes")) || [])
+  const [currentNoteId, setCurrentNoteId] = React.useState((notes[0] && notes[0].id) || "")
 
-  function createNewNote(){
+  React.useEffect(()=>{
+    localStorage.setItem("notes", JSON.stringify(notes))
+  },[notes])
+
+  function createNewNote() {
     const newNote = {
-      id : nanoid(),
+      id: nanoid(),
       body: "# Type your markdown note's title here"
     }
-    setNotes(prevNotes => [newNote,...prevNotes])
+    setNotes(prevNotes => [newNote, ...prevNotes])
     setCurrentNoteId(newNote.id)
+}
+
+  function updateNote(text) {
+    setNotes(oldNotes => oldNotes.map(oldNote => {
+        return oldNote.id === currentNoteId ? { ...oldNote, body: text } : oldNote
+    }))
+  }
+
+  function findCurrentNote() {
+      return notes.find(note => {
+        return note.id === currentNoteId
+      }) || notes[0]
   }
 
   return(
     <main>
-      {notes.length > 0
-      ?
-      <Split
-      sizes={[15, 85]} 
-      direction="horizontal"
-      className='split'
-    >
-      <Sidebar />
-      <Editor />
-    </Split> :
-    <section className='no-notes'>
-      <h2>You have no notes</h2>
-      <button 
-        className='first-note'
-        onClick={createNewNote}
-      >Create one now</button>
-    </section>
-    }
+      {
+        notes.length > 0 ? 
+        <Split 
+          sizes={[15,85]} 
+          direction="horizontal" 
+          className='split'
+        >
+          <Sidebar 
+            notes={notes} 
+            setCurrentNoteId={setCurrentNoteId} 
+            currentNote={findCurrentNote()} 
+            newNote={createNewNote} 
+          />
+          {currentNoteId && notes.length > 0 && 
+            <Editor 
+              updateNote={updateNote} 
+              currentNote={findCurrentNote()}
+            />
+          }
+        </Split>
+        :
+        <div className="no-notes">
+          <h1>You have no notes</h1>
+          <button 
+              className="first-note" 
+              onClick={createNewNote}
+          >
+            Create one now
+          </button>
+        </div>
+      }
     </main>
   )
 }
